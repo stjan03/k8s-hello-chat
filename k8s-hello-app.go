21dc7dfd476cd98f1bfe85dd9cd27ee1e116e267
Stefan Jansson <stefan.b.jansson@atea.se>
2021-07-17 17:53:23 +0200

    Update k8s-hello-app.go

--- k8s-hello-app.go
+++ k8s-hello-app.go
@@ -1,57 +1,88 @@
-// A webserver that publish...
-//	/		Default page the publish "Hello world... + $APP_TEXT"
-//	/data.json	Publish the pod specific enviroment data in json format
-//	/_healthz	Healthcheck "OK"
+// A simple webserver that written in golang that publish...
+//      /               Default page the publish "Hello world...
+//      /json           Publish the pod specific enviroment data in json format
+//      /health         Healthcheck "OK"
+//      /metrics        Prometheus metrics
 
-// The K8S_ is automaticlly set by the pod deployment
-// The APP_ is manually set
 
-// Linuxsmurfen
-// 2021-06-22
+// Enviroment varibles that can be set
+//    APP_* is manually set
+//    K8S_* is automaticlly set by the pod deployment
+
+// Created by Stefan
+// 2021-07-17
 
 // Compile with...
-// go build -o demo demo.go
+// go build k8s-hello-app.go
 
+
+// ----------- The magic code ------------------
 package main
 
 import (
         "fmt"
-        "log"
         "net/http"
         "os"
+        "log"
+        //"encoding/json"
+        "github.com/prometheus/client_golang/prometheus"
+        "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var response string
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello world..." + os.Getenv("APP_TEXT"))
+  fmt.Fprintln(w, "Hello world..." + os.Getenv("APP_TEXT"))
+  podInfo.Inc()
 }
 
-func jsonHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-        fmt.Fprintf(w, response)
-}
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "ok")
+  fmt.Fprintln(w, "ok")
 }
 
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+//      response := "{\n  \"Version\": \"" + os.Getenv("APP_VERSION") +
+//                    "\",\n  \"Text\": \"" + os.Getenv("APP_TEXT") +
+//                    "\",\n  \"Nodename\": \"" + os.Getenv("K8S_NODE_NAME") +
+//                    "\",\n  \"Podname\": \"" + os.Getenv("K8S_POD_NAME") +
+//                    "\",\n  \"Namespace\": \"" + os.Getenv("K8S_POD_NAMESPACE") +
+//                    "\",\n  \"PodIP\": \"" + os.Getenv("K8S_POD_IP") +
+//                    "\",\n  \"HostIP\": \"" + os.Getenv("K8S_HOST_IP") +
+//                    "\",\n  \"Serviceaccount\": \"" + os.Getenv("K8S_POD_SERVICE_ACCOUNT") +
+//                    "\",\n  \"Hostname\": \"" + os.Getenv("HOSTNAME") +
+//                    "\"\n}\n"
+  response := os.Environ()
+  fmt.Fprintf(w, response)
+}
+
+
+var (
+  podInfo = prometheus.NewCounter(prometheus.CounterOpts{
+    Name: "pod_info",
+    Help: "Pod information",
+    ConstLabels: prometheus.Labels{
+      "hostname":  os.Getenv("HOSTNAME"),
+      "appversion":  os.Getenv("APP_VERSION"),
+    },
+  })
+)
+
+
+func init() {
+  prometheus.MustRegister(podInfo)
+}
+
+
 func main() {
-        response = "{\n  \"Version\": \"" + os.Getenv("APP_VERSION") +
-                    "\",\n  \"Text\": \"" + os.Getenv("APP_TEXT") +
-                    "\",\n  \"Nodename\": \"" + os.Getenv("K8S_NODE_NAME") +
-                    "\",\n  \"Podname\": \"" + os.Getenv("K8S_POD_NAME") +
-                    "\",\n  \"Namespace\": \"" + os.Getenv("K8S_POD_NAMESPACE") +
-                    "\",\n  \"PodIP\": \"" + os.Getenv("K8S_POD_IP") +
-                    "\",\n  \"HostIP\": \"" + os.Getenv("K8S_HOST_IP") +
-                    "\",\n  \"Serviceaccount\": \"" + os.Getenv("K8S_POD_SERVICE_ACCOUNT") +
-                    "\"\n}\n"
+  http.HandleFunc("/", defaultHandler)
+  http.HandleFunc("/json", jsonHandler)
+  http.HandleFunc("/health", healthHandler)
+  http.Handle("/metrics", promhttp.Handler())
 
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/data.json", jsonHandler)
-    	http.HandleFunc("/_healthz", healthHandler)
-
-        log.Printf("Listening on :8080 at ...\n%s", response)
-        log.Fatal(http.ListenAndServe(":8080", nil))
+  log.Println("Listening on port 8080")
+  //log.Println(json.MarshalIndent(os.Environ()))
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
